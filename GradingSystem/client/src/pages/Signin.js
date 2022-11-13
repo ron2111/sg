472d9d2c22446be69b38a52341d7ec7718ce6b88
rs <siddycash@gmail.com>
2022-11-14 03:10:53 +0530

    login_update

--- GradingSystem/client/src/pages/Signin.js
+++ GradingSystem/client/src/pages/Signin.js
@@ -59,7 +59,7 @@ function App() {
                         <Col xs={12} className="d-flex align-items-center justify-content-center">
                             <div className="bg-white shadow-soft border rounded border-light p-4 p-lg-5 w-100 fmxw-500">
                                 <div className="text-center text-md-center mb-4 mt-md-0">
-                                    <h3 className="mb-0">Login with a Braude Account</h3>
+                                    <h3 className="mb-0">Login with a DTU Account</h3>
                                 </div>
                                 <center>
                                     <form onSubmit={handleSubmit} className="mt-4" >
